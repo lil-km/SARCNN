cefe802f8839ea6ac3f76690c2a4e4b3b3d5b8fe
Khalil <7khalil>
2021-12-26 00:07:25 +0100

    add script to generate training patches

---
+++ crop_sar.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+import argparse
+import numpy as np
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--inputs_dir', type = str, help='input directory')
+parser.add_argument('--outputs_dir', type = str, help='output directory')
+parser.add_argument('--patch_size', type = int, help='patch size')
+parser.add_argument('--stride', type = int, help='stride')
+parser.add_argument('--step', type = int, help='step')
+
+args = parser.parse_args()
+
+outputs_dir = Path(args.outputs_dir)
+outputs_dir.mkdir(parents = True, exist_ok = True)
+
+path = Path(args.inputs_dir)
+image_files = [*path.glob('*.npy')]
+
+print(f"number of training data {len(image_files)}")
+
+for f in image_files:
+    img = np.load(f)
+    H, W = img.shape
+    # make patches from the image
+    for x in range(0 + step, H - pat_size, stride):
+        for y in range(0 + step, W - pat_size, stride):
+            patch = img[x:x + pat_size, y:y + pat_size]
+            patch_name = f"{f.stem}_{x}_{y}.npy"
+            np.save(outputs_dir / patch_name, patch)
